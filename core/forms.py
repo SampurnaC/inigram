63dc78ae63657aebce4ca263eb379c0ec96c0375
sam <sampurna.chapagainnn@gmail.com>
2023-06-13 23:07:11 +0100

    add signin and signup forms with styling

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,35 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'form-control'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your Email',
+        'class': 'form-control'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Your Password',
+        'class': 'form-control'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your Password Confirmation',
+        'class': 'form-control'
+    }))
+    
     class Meta:
         model=User
         fields=('username', 'email', 'password1', 'password2')
 
+class SigninForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'form-control'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'form-control'
+    }))
