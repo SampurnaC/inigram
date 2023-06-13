8dc98486783cb81022798195e382c56738b5e603
sam <sampurna.chapagainnn@gmail.com>
2023-06-13 18:58:01 +0100

    add signup feature

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from core.forms import SignupForm
+from django.http import HttpResponse
 
-# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('signup/')
+    else:
+        form=SignupForm()    
+    return render(request, 'core/signup.html', {'form': form})
