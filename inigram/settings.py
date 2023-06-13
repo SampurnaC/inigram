9ee7f8dc7630c800e14b18a3be8fbac01f29430d
sam <sampurna.chapagainnn@gmail.com>
2023-06-13 23:08:16 +0100

    set values for login url and login redirect url

--- inigram/settings.py
+++ inigram/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL='/'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -123,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
