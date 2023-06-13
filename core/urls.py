f5ebf8f06acddec22fecbf156ef8803aca184e9b
sam <sampurna.chapagainnn@gmail.com>
2023-06-13 23:09:55 +0100

    add index, register and login urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
+from .forms import SigninForm
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup')
+    path('', views.index, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=SigninForm), name='login'),
 ]
