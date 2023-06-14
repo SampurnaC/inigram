d681584cec04a6c468cb53c39c2d9135df47c610
sam <sampurna.chapagainnn@gmail.com>
2023-06-14 11:14:43 +0100

    add create post view

--- inigramapp/views.py
+++ inigramapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post, User
+from .forms import PostForm
 
-# Create your views here.
+def new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post=form.save()
+            post.author=request.user
+            post.save()
+            return redirect('/')
+    else:
+        form=PostForm()
+    return render(request, 'inigramapp/new_post.html',{'form': form})            
