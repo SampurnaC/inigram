089ac7e08622a36ee2d48c6b56be1fe10b425a55
sam <sampurna.chapagainnn@gmail.com>
2023-06-15 13:31:52 +0100

    add styling for post form and remove created on field from meta class

--- inigramapp/forms.py
+++ inigramapp/forms.py
@@ -3,6 +3,16 @@ from .models import Post
 from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
+
+    title = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Enter your title',
+        'class': 'form-control'
+    }))
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': 'Enter Your description',
+        'class': 'form-control'
+    }))
+
     class Meta:
         model= Post
-        fields=['title', 'description', 'created_on']
+        fields=['title', 'description']
