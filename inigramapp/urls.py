f0d8ca443506efc4a9abf7b656b3d8fd4b619184
sam <sampurna.chapagainnn@gmail.com>
2023-06-15 15:03:41 +0100

    add route for edit and delete

--- inigramapp/urls.py
+++ inigramapp/urls.py
@@ -6,5 +6,7 @@ from .forms import PostForm
 urlpatterns = [
     path('new/', views.new, name='create-post'),
     path('show/<int:id>/', views.show, name='show-post'),
+    path('edit/<int:id>/', views.edit, name='edit-post'),
+    path('delete/<int:id>/', views.delete, name='delete-post'),
     
 ]
