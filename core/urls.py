c700fd32f261bc71682a793e6f5079e0f5308ccd
sam <sampurna.chapagainnn@gmail.com>
2023-06-18 23:18:19 +0100

    add urls for profile and profile edit page

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=SigninForm), name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('profile/edit/<int:id>/', views.profile_edit, name='edit-profile'),
+    path('profile/', views.profile, name='profile'),
+
 
 ]
