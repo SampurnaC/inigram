1bfcd69f80653d38b4eb3e3922c22516b03fa0e4
sam <sampurna.chapagainnn@gmail.com>
2023-06-18 23:20:52 +0100

    add views for profile and profile_edit

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
-from core.forms import SignupForm
+from core.forms import SignupForm, EditProfileForm
 from django.http import HttpResponse
 from django.contrib.auth import logout
+from .models import User
+
+from core.models import Profile
 from inigramapp.models import Post
 
 def signup(request):
@@ -16,9 +19,22 @@ def signup(request):
 
 def index(request):
     posts = Post.objects.all().order_by('-created_on')
+
     return render(request, 'core/index.html', {'posts': posts})
 
 def logout_user(request):
     logout(request)
     return redirect('/')
 
+def profile_edit(request, id):
+    profile=Profile.objects.get(id=id)
+    form=EditProfileForm(request.POST or None, instance=profile)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'core/profile_edit.html', {'profile': profile, 'form': form})
+
+def profile(request):
+    profile_posts = Post.objects.filter(author=request.user)
+
+    return render(request, 'core/profile.html', {'profile_posts': profile_posts})
