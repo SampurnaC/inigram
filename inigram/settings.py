f866656bdb3aedae5288f087224ee2116b5925be
sam <sampurna.chapagainnn@gmail.com>
2023-07-11 23:24:53 +0100

    fixes

--- inigram/settings.py
+++ inigram/settings.py
@@ -122,17 +122,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# CKEditor Settings
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_IMAGE_BACKEND = "pillow"
 CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js' 
@@ -144,6 +146,7 @@ CKEDITOR_CONFIGS = {
             'width': 'auto',
             'extraPlugins': ','.join([
                 'codesnippet',
+                # 'youtube'
             ]),
         },
 }
