894053d7141440c8885106b1fad0bc05c7a72630
sam <sampurna.chapagainnn@gmail.com>
2023-07-11 13:38:13 +0100

    migration changes

--- inigramapp/models.py
+++ inigramapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title=models.CharField(max_length=50)
